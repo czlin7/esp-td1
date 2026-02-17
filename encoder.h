adf6746f564a65df50ca266fbd8af591f3752ff6
milize86 <chanwoojang@outlook.com>
2026-02-17 15:10:39 +0000

    Add files via upload

--- encoder.h
+++ encoder.h
@@ -1,21 +1,20 @@
-#pragma once
-
 #include "mbed.h"
 #include "QEI.h"
-
-static float M_PI = 3.14159265359f;
+#include <cmath>
 
 class WheelEncoder {
     private:
         QEI enc;
         Timer t;
         float functionVal;
+        float finalCPR;
         int previousTick;
         float previousTime;
         float velocity;
+        float rpm;
     public:
         WheelEncoder(PinName A, PinName B, PinName index, float diameter, float gear_ratio, int cpr) : enc(A, B, index, cpr, QEI::X2_ENCODING), t() {
-            float finalCPR = cpr * 2.0f; //effective cpr for x2 encoding mode
+            finalCPR = cpr * 2.0f; //effective cpr for x2 encoding mode
             functionVal = (M_PI * diameter) / (finalCPR * gear_ratio); //calculates the value for function f(x,y,z)
 
             //reset everything once
@@ -39,12 +38,18 @@ class WheelEncoder {
 
             float tickRate = elapsedTick / elapsedTime; //ticks per second
             velocity = tickRate * functionVal; //meter per second
+            rpm = (tickRate / finalCPR) * 60.0f;
 
             previousTick = currentTick; //save the final tick value at the termination of this function
             previousTime = currentTime; //save the final time value at the termination of this function
 
             return velocity;
         }
+
+        float getRPM(void){
+            return rpm;
+        }
+
 
         void reset() {
             enc.reset();
