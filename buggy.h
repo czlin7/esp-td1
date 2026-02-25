f3eb01c2082c5f6b3245f91e589efdc4b87b889e
aydinjefri <muhdaydin11@gmail.com>
2026-02-25 21:46:31 +0000

    Refactor UIController into Buggy class with motor control

--- buggy.h
+++ buggy.h
@@ -1,220 +1,111 @@
 #pragma once
 
-#include "mbed.h"
-#include "C12832.h"
 #include "motors.h"
-#include "potentiometer.h"
-#include "states.h"
-#include "buggy.h"
+#include "encoder.h"
 
-// UI control states
-typedef struct {
-    UiState state;
-    UiFocus focus;
-} UI;
 
-//UI controller class
-class UIController {
+class Buggy {
 private:
-    UI m_ui;
+    Motor *leftMotor;
+    Motor *rightMotor;
+    WheelEncoder *leftEncoder;
+    WheelEncoder *rightEncoder;
+    DigitalOut enable;
 
-    C12832 *m_lcd;
-    MotorData *m_motor_sig_L;
-    MotorData *m_motor_sig_R;
-    SamplingPotentiometer *m_potL;
-    SamplingPotentiometer *m_potR;
-    InterruptIn *m_button;
-    InterruptIn *m_lbutton;
-    InterruptIn *m_rbutton;
-    DigitalOut *m_ledR;
-    DigitalOut *m_ledG;
-    DigitalOut *m_ledB;
-    Buggy *buggy;
-
-    volatile bool m_toggleRequested;
-    volatile bool m_LeftRequested;
-    volatile bool m_RightRequested;
-    UiFocus m_prevFocus;
-
-    void setColor(float r, float g, float b) {
-        *m_ledR = r;
-        *m_ledG = g;
-        *m_ledB = b;
-    }
-
-    void updateModeLED() {
-        if (m_ui.state == Navigation)
-            setColor(0,0,0);     // Navigation mode
-        else
-            setColor(0,1,1);     // Edit mode
-    }
-
-    void updateSelector() {
-
-        if (m_ui.focus == m_prevFocus) return;
-
-        // erase old rectangle
-        switch (m_prevFocus) {
-            case Status: m_lcd->fillrect(0, 0, 69, 12, 0); break;
-            case Duty:   m_lcd->fillrect(0, 20, 69, 31, 0); break;
-            case Mode:   m_lcd->fillrect(70, 0, 126, 12, 0); break;
-            case Dir:    m_lcd->fillrect(70, 20, 126, 31, 0); break;
-            case Speed:  break;
-        }
-
-        // draw new rectangle
-        switch (m_ui.focus) {
-            case Status: m_lcd->rect(0, 0, 69, 12, 1); break;
-            case Duty:   m_lcd->rect(0, 20, 69, 31, 1); break;
-            case Mode:   m_lcd->rect(70, 0, 126, 12, 1); break;
-            case Dir:    m_lcd->rect(70, 20, 126, 31, 1); break;
-            case Speed:  break;
-        }
-
-        m_prevFocus = m_ui.focus;
-    }
-    enum MotorSelect {
-            MOTOR_LEFT,
-            MOTOR_RIGHT
-        };
-
-        MotorSelect m_selectedMotor;
-        MotorData *m_motorL;
-        MotorData *m_motorR;
-        MotorData *m_activeMotor;   // pointer to whichever is selected
 public:
-    UIController(C12832 *lcd,
-                 MotorData *motor_sig_L,
-                 MotorData *motor_sig_R,
-                 SamplingPotentiometer *potL,
-                 SamplingPotentiometer *potR,
-                 InterruptIn *button,
-                 InterruptIn *Lbutton,
-                 InterruptIn *Rbutton,
-                 DigitalOut *r,
-                 DigitalOut *g,
-                 DigitalOut *b,
-                 Buggy *buggy)
-        : m_lcd(lcd),
-          m_motor_sig_L(motor_sig_L),
-          m_motor_sig_R(motor_sig_R),
-          m_motorL(motor_sig_L),
-          m_motorR(motor_sig_R),
-          m_potL(potL),
-          m_potR(potR),
-          m_button(button),
-          m_lbutton(Lbutton),
-          m_rbutton(Rbutton),
-          m_ledR(r),
-          m_ledG(g),
-          m_ledB(b),
-          buggy(buggy),
-          m_toggleRequested(false),
-          m_prevFocus(Status)
-    {
-        m_selectedMotor = MOTOR_LEFT;
-        m_activeMotor = m_motorL;
-        m_ui.state = Navigation;
-        m_ui.focus = Status;
+    Buggy(Motor *L, Motor *R, WheelEncoder *LE, WheelEncoder *RE, PinName en) : leftMotor(L), rightMotor(R), leftEncoder(LE), rightEncoder(RE), enable(en) {}
 
-        m_button->rise(callback(this, &UIController::onButtonISR));
-        m_lbutton->rise(callback(this, &UIController::onLeftISR));
-        m_rbutton->rise(callback(this, &UIController::onRightISR));
-        updateModeLED();
+    void setEnable(bool enable_signal) {
+        enable.write(enable_signal);
     }
 
-
-    void onButtonISR() {
-        m_toggleRequested = true;
+    bool getEnable() {
+        return enable.read();
     }
 
-    void onLeftISR() {
-        m_LeftRequested = true;
+    void forward_backward(float moving_speed, float time_fb){
+        leftMotor->move(moving_speed);
+        rightMotor->move(moving_speed);
+        wait(time_fb);
+        leftMotor->move(0.0f);
+        rightMotor->move(0.0f); //stops the buggy after the wait finishes
     }
 
-    void onRightISR() {
-        m_RightRequested = true;
+    void turn_left_right(float turning_speed, float time_lr){ //ensure that speed value is > 0 to turn right, and < 0 to turn left
+        leftMotor->move(turning_speed);
+        rightMotor->move(-turning_speed);
+        wait(time_lr);
+        leftMotor->move(0.0f);
+        rightMotor->move(0.0f); //stops the buggy after the wait finishes
     }
 
-    void processButton() {
-        if (m_toggleRequested) {
-            m_toggleRequested = false;
-            m_ui.state = (m_ui.state == Navigation) ? Edit : Navigation;
+    void moveDistance(float targetDistance_m, float moving_speed){
+        // Reset encoders to measure counts for desired distance
+        leftEncoder->reset();
+        rightEncoder->reset();
+
+        //Set bipolarity and direction
+
+        // Set the desired speed to move
+        enable.write(1);
+        leftMotor->move(moving_speed);
+        rightMotor->move(moving_speed);
+
+        // // Moving while loop while measuring count/distance using encoder
+        while (true)
+        {
+            float distL = leftEncoder->getDistance();
+            float distR = rightEncoder->getDistance();
+            float avgDist = (distR + distL) * 0.5 ;
+            if (avgDist > targetDistance_m)
+            break;
+
+        wait(0.0004f); //2500Hz Distance loop
         }
-        updateModeLED();
+        // Stop motor after desired distance already travelled
+        enable.write(0);
+        leftMotor->move(0);
+        rightMotor->move(0);
+       
     }
 
-    void processMotorSelection() {
-    if (m_LeftRequested) {
-        m_LeftRequested = false;
-        m_selectedMotor = MOTOR_LEFT;
-        m_activeMotor = m_motorL;
-    }
+    void rotateAngle(float angle_deg, float speed){
+        const float PI = 3.14159265359f;
+        float angle_rad = angle_deg * PI/180.0f;
+        const float wheelBase = 0.16f; // Measure and enter the value
 
-    if (m_RightRequested) {
-        m_RightRequested = false;
-        m_selectedMotor = MOTOR_RIGHT;
-        m_activeMotor = m_motorR;
-    }
-}
+        // Distance targeted for the desired rotation? or turning?
+        float targetDistance = (angle_rad * wheelBase) / 2.0f;
 
+        // Reset encoders to measure counts for turning desired degree
+        leftEncoder->reset();
+        rightEncoder->reset();
 
-    // Function to render display of UI
-    void renderDisplay() {
+        // Determine direction of turning
+        float leftSpeed = (angle_deg > 0) ? speed : -speed;
+        float rightSpeed = (angle_deg > 0) ? -speed : speed;
 
-        updateSelector();
+        // Actaute motor to turn at desired speed
+        enable.write(1);
+        leftMotor->move(leftSpeed);
+        rightMotor->move(rightSpeed);
 
-        m_lcd->locate(1, 11);
-        m_lcd->printf("%s", 
-        m_selectedMotor == MOTOR_LEFT ? "Left" : "Right");
+        // Turning while loop while measuring count/degree of turning using encoder
+        while(true)
+        {
+            float distL = fabs(leftEncoder->getDistance());
+            float distR = fabs(rightEncoder->getDistance());
+            float avgDist = (distL + distR) * 0.5f;
+            //printf("L %.3f R %.3f\n", distL, distR);
+            if (avgDist >= fabs(targetDistance))
+            break;
 
-
-        m_lcd->locate(1, 1);
-        m_lcd->printf("Status:%s",
-                      buggy->getEnable() ? "Enabled" : "Disabled");
-
-        m_lcd->locate(1, 21);
-        m_lcd->printf("Duty Cycle:%0.1f",
-                      m_activeMotor->duty_cycle);
-
-        m_lcd->locate(71, 1);
-        m_lcd->printf("Mode:%s",
-                      m_activeMotor->motor_bipolar ? "Bi" : "Uni");
-
-        m_lcd->locate(71, 21);
-        m_lcd->printf("Direction:%s",
-                      m_activeMotor->motor_dir ? "Fw" : "Bw");
-    }
-
-    void handleNavigation() {
-
-        float navValue  = m_potL->getCurrentSampleNorm();
-        float editValue = m_potR->getCurrentSampleNorm();
-
-        // Navigation mode for menu
-        if (m_ui.state == Navigation) {
-
-            if      (navValue < 0.2f) m_ui.focus = Status;
-            else if (navValue < 0.4f) m_ui.focus = Duty;
-            else if (navValue < 0.6f) m_ui.focus = Speed;
-            else if (navValue < 0.8f) m_ui.focus = Mode;
-            else                     m_ui.focus = Dir;
+        wait(0.0004f);
         }
 
-        // Edit mode
-        if (m_ui.state == Edit) {
-
-            if (m_ui.focus == Duty)
-                m_activeMotor->duty_cycle = editValue;
-
-            if (m_ui.focus == Dir)
-                m_activeMotor->motor_dir = (editValue >= 0.5f);
-
-            if (m_ui.focus == Mode)
-                m_activeMotor->motor_bipolar = (editValue >= 0.5f);
-
-            if (m_ui.focus == Status)
-                buggy->setEnable(editValue >= 0.5f);
-        }
+        // Stop motor once desired angle has reached
+        enable.write(0);
+        leftMotor->move(0);
+        rightMotor->move(0);
     }
 };
